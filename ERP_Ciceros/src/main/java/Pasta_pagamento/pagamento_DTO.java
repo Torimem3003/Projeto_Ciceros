3042ce6a464b11602b6c355fa2ec589a2001ea60
Capuxs <caua.benachio5@gmail.com>
2024-05-03 22:32:56 -0300

    funcionalidade pagamento
    Fiz a tela de consultar pagamentos e tambem o CRUD consultar e salvar.

--- ERP_Ciceros/src/main/java/Pasta_pagamento/pagamento_DTO.java
+++ ERP_Ciceros/src/main/java/Pasta_pagamento/pagamento_DTO.java
@@ -1,10 +1,81 @@
 
 package Pasta_pagamento;
+import java.time.LocalDate;
 
-/**
- *
- * @author vsant
- */
 public class pagamento_DTO {
-    
+    private String formaDePagamento = "";
+    private double valorPedido = 0;
+    private LocalDate data;
+    private int idPedido = 0;
+    private int idPagamento = 0;
+
+    /**
+     * @return the formaDePagamento
+     */
+    public String getFormaDePagamento() {
+        return formaDePagamento;
+    }
+
+    /**
+     * @param formaDePagamento the formaDePagamento to set
+     */
+    public void setFormaDePagamento(String formaDePagamento) {
+        this.formaDePagamento = formaDePagamento;
+    }
+
+    /**
+     * @return the valorPedido
+     */
+    public double getValorPedido() {
+        return valorPedido;
+    }
+
+    /**
+     * @param valorPedido the valorPedido to set
+     */
+    public void setValorPedido(double valorPedido) {
+        this.valorPedido = valorPedido;
+    }
+
+    /**
+     * @return the data
+     */
+    public LocalDate getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the idPedido
+     */
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    /**
+     * @param idPedido the idPedido to set
+     */
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    /**
+     * @return the idPagamento
+     */
+    public int getIdPagamento() {
+        return idPagamento;
+    }
+
+    /**
+     * @param idPagamento the idPagamento to set
+     */
+    public void setIdPagamento(int idPagamento) {
+        this.idPagamento = idPagamento;
+    }
 }
